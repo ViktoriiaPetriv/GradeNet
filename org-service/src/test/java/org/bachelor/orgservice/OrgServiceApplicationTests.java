4cb725ac9fd978fc835a49c6856c80d34caa27b9
Viktoriia Petriv <vika.petriv.05@gmail.com>
2026-02-04 23:13:54 +0200

    Init project with docker file for org-service

---
+++ org-service/src/test/java/org/bachelor/orgservice/OrgServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bachelor.orgservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
